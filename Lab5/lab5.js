099ea8753ee41aec1b655b421f138b22df613fdc
Amensisa W <98834478+Amenu70@users.noreply.github.com>
2023-05-05 15:05:46 -0500

    Updated Lab 5 Links

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -1,18 +1,23 @@
+// Question 1
 function max(x1, x2) {
     if (x1 > x2) return x1;
     else return x2;
 }
 
+// Question 2
 function maxOfThree(x1, x2, x3) {
     return x1 > x2 ? (x1 > x3 ? x1 : x3) : (x2 > x3 ? x2 : x3);
 }
 
+// Question 3
 function isVowel(character) {
     if (character == "a" || "e" || "i" || "o" || "u")
         return true;
     else    
         return false;
 }
+
+// Question 4
 function sum(array) {
     let sum = 0;
     for (let i = 0; i < array.length;i++) {
@@ -29,6 +34,7 @@ function multiply(array) {
     return prod;
 }
 
+// Question 5
 function reverse(string) {
     let reversedString = "";
     for (let i = string.length; i >= 0; i--){
@@ -36,26 +42,38 @@ function reverse(string) {
     }
     return reversedString;
 }
+
+// Question 6
 function findLongestWord(array) {
     return array.map(item => item.length)
         .reduce((previous, current) => current > previous ? current : previous, 0);
 }
+
+// Question 7
 function findLongestWords(array, number) {
     return array.filter(item => item.length > number);
 }
+
+// Question 8
 function computeSumofSquares(array) {
     return array.map(item => item * item)
         .reduce((sum, current) => sum + current, 0);
 }
+
+// Question 9
 function printOddNumbersOnly(array) {
     return array.filter(item => item % 2 != 0);
 }
+
+// Question 10
 function computeSumofSquaresOfEvenOnly(array) {
     return array
         .filter(item => item % 2 == 0)
         .map(item => item * item)
         .reduce((sum, current) => sum + current, 0);
 }
+
+// Question 11
 function sumfunctional(array) {
     return array.reduce((sum, current) => sum + current, 0);
 }
@@ -63,6 +81,9 @@ function sumfunctional(array) {
 function multiplyfunctional(array) {
     return array.reduce((prod, current) => prod *current, 1);
 }
+
+
+// Question 12
 function printFibonacci(n, a, b) {
     var out = [a,b];
     for (let i = 0; i <n-2; i++){
